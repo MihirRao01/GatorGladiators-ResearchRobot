2c101f1092985e47bc05dcc424f0635665ce5b7a
MihirRao01 <mihirhrao@gmail.com>
2024-02-04 13:28:26 -0500

    integrating the functionality in fetch arduino.py into client.py

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
-import socket 
+import socket
+import serial.tools.list_ports
 
-jetson_server = "192.168.56.1"
-#jetson_server = "104.222.18.161"
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial("/dev/cu.usbmodem14301", 9600)
+
+# jetson_server = "192.168.56.1"
+# jetson_server = "104.222.18.161"
+jetson_server = "100.110.218.93"
 jetson_server_port = 8000
 ADDR = (jetson_server, jetson_server_port)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
 
 def send(data):
     msg = data.encode('utf-8')
@@ -17,5 +23,14 @@ def send(data):
     client.send(msg)
 
 
-send("Hello World!")
-send("Disconnected")
+send("Hello JetsonNano!")
+
+while True:
+    try : 
+        if serialInst.in_waiting:
+            packet = serialInst.readline()
+            print(packet.decode('utf'))
+            send(packet.decode('utf'))
+    except:
+        send("Disconnected")
+        break
